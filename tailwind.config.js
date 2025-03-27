0fea3c2b17a5c82be77dfd8378ea29ac9f2f7046
webkubor <webkubor@163.com>
2025-03-27 16:27:32 +0800

    feat  提交样式优化

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,31 @@ export default {
     extend: {
       colors: {
         brand: {
-          DEFAULT: '#FF4500', // Orange-red color for 3cdepot
-        }
-      }
+          DEFAULT: '#FF4500',
+          50: '#FFF2EC',
+          100: '#FFE5D9',
+          200: '#FFCCB3',
+          300: '#FFB38C',
+          400: '#FF9966',
+          500: '#FF8040',
+          600: '#FF6619',
+          700: '#FF4D00',
+          800: '#CC3D00',
+          900: '#992E00',
+        },
+        gray: {
+          50: '#F9FAFB',
+          100: '#F3F4F6',
+          200: '#E5E7EB',
+          300: '#D1D5DB',
+          400: '#9CA3AF',
+          500: '#6B7280',
+          600: '#4B5563',
+          700: '#374151',
+          800: '#1F2937',
+          900: '#111827',
+        },
+      },
     },
   },
   plugins: [],
